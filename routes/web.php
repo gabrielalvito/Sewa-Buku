d07bcfd4670404121b290a0a63968a014dec1e84
gabrielalvito <alvitoadinugraha@gmail.com>
2022-06-23 10:47:07 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -13,122 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\IndexController@index');
-
-
-Route::get('home', function () {
-    return view ('home');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('data_peminjam', 'App\Http\Controllers\DataPeminjamController@index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('data_peminjam/create', 'App\Http\Controllers\DataPeminjamController@create')->name('data_peminjam.create');
-
-Route::post('data_peminjam/store', 'App\Http\Controllers\DataPeminjamController@store')->name('data_peminjam.store');
-
-Route::get('lihat_data_peminjam', 'App\Http\Controllers\PeminjamController@lihat_data_peminjam');
-
-Route::get('data_peminjam/edit/{id}', 'App\Http\Controllers\DataPeminjamController@edit')->name('data_peminjam.edit');
-
-Route::post('data_peminjam/update/{id}', 'App\Http\Controllers\DataPeminjamController@update')->name('data_peminjam.update');
-
-Route::post('data_peminjam/delete/{id}', 'App\Http\Controllers\DataPeminjamController@destroy')->name('data_peminjam.destroy');
-
-Route::get('coba_collection', 'App\Http\Controllers\DataPeminjamController@CobaCollection');
-
-Route::get('collection_first', 'App\Http\Controllers\DataPeminjamController@collection_first');
-
-Route::get('collection_last', 'App\Http\Controllers\DataPeminjamController@collection_last');
-
-Route::get('collection_count', 'App\Http\Controllers\DataPeminjamController@collection_count');
-
-Route::get('collection_take', 'App\Http\Controllers\DataPeminjamController@collection_take');
-
-Route::get('collection_pluck', 'App\Http\Controllers\DataPeminjamController@collection_pluck');
-
-Route::get('collection_where', 'App\Http\Controllers\DataPeminjamController@collection_where');
-
-Route::get('collection_wherein', 'App\Http\Controllers\DataPeminjamController@collection_wherein');
-
-Route::get('collection_toarray', 'App\Http\Controllers\DataPeminjamController@collection_toarray');
-
-Route::get('collection_tojson', 'App\Http\Controllers\DataPeminjamController@collection_tojson');
-
-Route::get('peminjaman', 'App\Http\Controllers\PeminjamanController@index');
-
-Route::get('peminjaman/create', 'App\Http\Controllers\PeminjamanController@create')->name('peminjaman.create');
-
-Route::post('peminjaman/store', 'App\Http\Controllers\PeminjamanController@store')->name('peminjaman.store');
-
-Route::get('peminjaman/detail_peminjam/{id}', 'App\Http\Controllers\PeminjamanController@detail_peminjam')->name('peminjaman.detail_peminjam');
-
-Route::get('peminjaman/detail_buku/{id}', 'App\Http\Controllers\PeminjamanController@detail_buku')->name('peminjaman.detail_buku');
-
-Route::get('data_peminjam/search', 'App\Http\Controllers\DataPeminjamController@search')->name('data_peminjam.search');
-
-// Route::get('biodata', function(){
-//     return 'Nama : Vito <br> Nim : 43320011 <br> Alamat : Pedurungan, Semarang <br> Hobi : Turu';
-// });
-
-// Route::get('mahasiswa/{prodi}', function ($prodi) {
-//     return 'Mahasiswa Program Studi : '.$prodi;
-// });
-
-// Route::get('mahasiswa2/{prodi?}', function ($prodi=null) {
-//     if($prodi == null)
-//         return "Data Program Studi Kosong";
-//     return "Mahasiswa Program Studi : ".$prodi;
-// });
-
-// Route::get('mahasiswa3/{prodi?}', function ($prodi = "Teknologi Rekayasa Komputer") {
-//     return 'Mahasiswa Prodi : '.$prodi;
-// });
-
-// Route::get('biodata2', function(){
-//     return view('biodata2');
-// });
-
-// Route::group([], function(){
-//     Route::get('/pertama', function(){
-//         echo "route pertama";
-//     });
-//     Route::get('/kedua', function(){
-//         echo "route kedua";
-//     });
-//     Route::get('/ketiga', function(){
-//         echo "route ketiga";
-//     });
-// });
-
-// Route::group(['prefix' => 'latihan'], function(){
-//     Route::get('/satu', function(){
-//         echo "Latihan 1";
-//     });
-//     Route::get('/dua', function(){
-//         echo "Latihan 2";
-//     });
-//     Route::get('/tiga', function(){
-//         echo "Latihan 3";
-//     });
-// });
-
-// Route::group(array('prefix' =>'admin'), function(){
-//     // url ke home
-//     Route::get('/', function(){
-//         return 'Halaman Home Admin';
-//     });
-//     // route ke input data buku
-//     Route::get('posts', function(){
-//         return 'Halaman Input Data Buku';
-//     });
-//     // route ke halaman menyimpan data
-//     Route::get('posts/simpan', function(){
-//         return 'Data Berhasil Disimpan';
-//     });
-// });
-
-// Route::name('kuliah')->group(function(){
-//     Route::get('Teknologi Rekayasa Komputer', function(){
-//         return "Kuliah di Program Studi Teknologi Rekayasa Komputer";
-//     });
-// });
+require __DIR__.'/auth.php';
